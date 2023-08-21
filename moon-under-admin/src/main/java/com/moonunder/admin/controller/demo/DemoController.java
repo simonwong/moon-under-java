fdbeaf87fb5308dc837c0666732269e9f079c0ef
Simon <wsj_simon@163.com>
2023-08-21 09:39:13 +0800

    feat: add demo service

--- moon-under-admin/src/main/java/com/moonunder/admin/controller/demo/DemoController.java
+++ moon-under-admin/src/main/java/com/moonunder/admin/controller/demo/DemoController.java
@@ -1,6 +1,8 @@
 package com.moonunder.admin.controller.demo;
 
+import com.moonunder.admin.service.demo.DemoService;
 import com.moonunder.common.core.dto.ResponseDTO;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/demo")
+@RequiredArgsConstructor
 public class DemoController {
+  private final DemoService demoService;
+
   @GetMapping
   public ResponseDTO<String> getDemo() {
-    return ResponseDTO.ok("Hello Demo");
+    String txt = demoService.getDemo();
+    return ResponseDTO.ok(txt);
   }
 }
---
+++ moon-under-admin/src/main/java/com/moonunder/admin/service/demo/DemoService.java
@@ -0,0 +1,10 @@
+package com.moonunder.admin.service.demo;
+
+/**
+ * @author simon
+ */
+public class DemoService {
+  public String getDemo() {
+    return "Hello Moon Under";
+  }
+}
